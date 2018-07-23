fd592701966a04f446bf77005b3bf4da6a3bc2fd
ryucs <ryucs@coontec.com>
2018-07-23 16:24:13 +0900

    initial commit

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+/*!
+ * helloworld sample
+ * version: 1.0-2018.07.23
+ * Copyright (c) 2018 ryucs
+ * Project repository: https://github.com/ryucs/helloworld
+ * Licensed under the RPL license.
+ */
+
+#include <stdio.h>
+ 
+void main(void)
+{
+	printf("hello,world!\n");
+}
